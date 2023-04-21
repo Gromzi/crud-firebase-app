0847b19fde88c78fecc83882c534feb580bea7d3
Gromzi <mamgier@gmail.com>
2023-04-21 12:55:13 +0200

    Set up firebase and other dependencies

---
+++ src/config/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCNXzCP_CMPx_34a6Z2Z6gGZYqVVvcmR_w',
+  authDomain: 'crud-firebase-app-43d88.firebaseapp.com',
+  projectId: 'crud-firebase-app-43d88',
+  storageBucket: 'crud-firebase-app-43d88.appspot.com',
+  messagingSenderId: '555287743553',
+  appId: '1:555287743553:web:9c01f248933567e7aa8ac0',
+  measurementId: 'G-XRN0WDCV86',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
